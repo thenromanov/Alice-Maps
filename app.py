cd79ba52b0b71081e7c66bf86082b56d71c63b2a
Nikita <thenromanov@gmail.com>
2020-04-25 19:28:42 +0300

    Added user naming

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ app = Flask(__name__)
 
 logging.basicConfig(level=logging.INFO)
 
+sessionStorage = {}
+
 
 @app.route('/', methods=['POST'])
 def main():
@@ -21,20 +23,37 @@ def main():
 
 
 def handleDialog(res, req):
+    userId = req['session']['user_id']
     if req['session']['new']:
-        res['response']['text'] = 'Привет! Я могу показать город или сказать расстояние между городами!'
+        res['response']['text'] = 'Привет! Назови своё имя!'
+        sessionStorage[userId] = {'name': None}
         return
-    cities = getCities(req)
-    if not cities:
-        res['response']['text'] = 'Ты не написал не одного города!'
-    elif len(cities) == 1:
-        res['response']['text'] = f'Этот город в стране {getGeoInfo(cities[0], "country")}'
-    elif len(cities) == 2:
-        dist = getDistance(getGeoInfo(cities[0], 'coordinates'),
-                           getGeoInfo(cities[1], 'coordinates'))
-        res['response']['text'] = f'Расстояние между этими городами: {str(round(dist))} км'
+    if sessionStorage[userId]['name'] is None:
+        name = getName(req)
+        if name is None:
+            res['response']['text'] = 'Не расслышала имя. Повтори, пожалуйста!'
+        else:
+            sessionStorage[userId]['name'] = name
+            res['response']['text'] = f'Приятно познакомиться, {name.title()}. Я - Алиса. Я могу показать город или сказать расстояние между городами!'
     else:
-        res['response']['text'] = 'Слишком много городов!'
+        name = sessionStorage[userId]['name'].title()
+        cities = getCities(req)
+        if not cities:
+            res['response']['text'] = name + ', ты не написал не одного города!'
+        elif len(cities) == 1:
+            res['response']['text'] = f'{name}, этот город в стране {getGeoInfo(cities[0], "country")}'
+        elif len(cities) == 2:
+            dist = getDistance(getGeoInfo(cities[0], 'coordinates'),
+                               getGeoInfo(cities[1], 'coordinates'))
+            res['response']['text'] = f'{name}, расстояние между этими городами: {str(round(dist))} км'
+        else:
+            res['response']['text'] = name + ', слишком много городов!'
+
+
+def getName(req):
+    for entity in req['request']['nlu']['entities']:
+        if entity['type'] == 'YANDEX.FIO':
+            return entity['value'].get('first_name', None)
 
 
 def getCities(req):
